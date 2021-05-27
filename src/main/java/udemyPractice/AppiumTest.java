d9ff236f1a35bf29f00386bc18bf226559b4e67a
=Kamal <=>
2021-05-27 12:44:56 -0400

    first commit

---
+++ src/main/java/udemyPractice/AppiumTest.java
@@ -0,0 +1,21 @@
+package udemyPractice;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+    @Test
+    public void NativeAppAndroid (){
+
+        System.out.println("NativeAppAndroid");
+
+    }
+    @Test
+    public void iosApps (){
+        System.out.println("iosApps");
+
+    }
+
+
+
+
+}
---
+++ src/main/java/udemyPractice/RestAPITest.java
@@ -0,0 +1,19 @@
+package udemyPractice;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+    @Test
+    public void postJira (){
+        System.out.println("postJira");
+
+
+
+    }
+    @Test
+    public void deleteTweeter (){
+        System.out.println("deleteTweeter");
+
+    }
+
+}
